9aeb1f09c71cb3665e2d86a9cd065ebb48ebe210
Katarina <katarinarosiak@gmail.com>
2020-08-25 13:58:09 +0200

    json file with messages

---
+++ README.md
@@ -0,0 +1,3 @@
+Tic Tac Toe
+
+This game is a project which is a part of Launch School Curriculum. The whole game was designed to be used at console. 
---
+++ tic_tac_toe_console.js
@@ -0,0 +1,22 @@
+const readline = require.readlin('sync');
+const messages = require('./messages_tic_tac_toe.jsson')
+
+//start the first loop
+//info: sign? computer? player? name?
+//display the board 
+//second game loop 
+// choice
+//print
+//check
+//choice 
+//until let taken = 9 || win = true
+// again? 
+
+print(messages.welcome);
+
+
+//helper functions
+function print(message) {
+  console.log(`=> ${message}`);
+}
+
