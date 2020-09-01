3aeb5627cffb7810702ea6735fb1afe1604ee1c2
Katarina <katarinarosiak@gmail.com>
2020-09-01 21:28:53 +0200

    working on minimax algoruthm for smart IA

--- tic_tac_toe_console.js
+++ tic_tac_toe_console.js
@@ -9,7 +9,7 @@ let signs = {
   circle: 'O'
 }
 
-let WIN_COMBINATION = [
+const WIN_COMBINATION = [
   ['a1', 'a2', 'a3'], ['b1', 'b2', 'b3'], ['c1', 'c2', 'c3'],
   ['a1', 'b1', 'c1'], ['a2', 'b2', 'c2'], ['a3', 'b3', 'c3'],
   ['a1', 'b2', 'c3'], ['c1', 'b2', 'a3']
