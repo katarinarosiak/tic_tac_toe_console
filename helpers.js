09726012e1d6b39b11561ffc2579ae53dde1c33d
Katarina <katarinarosiak@gmail.com>
2020-09-06 16:01:19 +0200

    tic tac toe console complete with minimax algo

--- helpers.js
+++ helpers.js
@@ -1,5 +1,4 @@
 const readline = require('readline-sync');
-const messages = require('./messages_tic_tac_toe.json');
 const VALID_ANSWERS = require('./valid_answers.json');
 
 
@@ -67,7 +66,6 @@ function humanChooseSquare(messages, board, initialMarker, validAnswers, name) {
   let chosenSquare = retriveInput(messages.chooseSquare, messages.ivalidSquare, VALID_ANSWERS.validSquares, name);
 
   while (board[chosenSquare] !== initialMarker) {
-
     print(messages.emptySquare, name);
     chosenSquare = retriveInput(messages.chooseSquare, messages.ivalidSquare, validAnswers.validSquares, name);
   }
@@ -132,7 +130,7 @@ function changeTurn(game, messages, players) {
   print(messages.YourTurn, game.currentPlayer.name);
 }
 
-////////////////////////////////////////////////////////
+
 function computerChooseSquareEasy(game, sign) {
   let squares = ['a1', 'a2', 'a3', 'b1', 'b2', 'b3', 'c1', 'c2', 'c3'];
   let computerChoice = randomChooseBetween(squares);
@@ -158,8 +156,8 @@ function restartGame(game, players) {
   choosePlayerForFirstMove(players, game);
 }
 
-function choosePlayerForFirstMove(players, game) {
 
+function choosePlayerForFirstMove(players, game) {
   if (game.round % 2 === 1) {
     game.currentPlayer = players.player1;
   } else {
@@ -194,29 +192,6 @@ function initializeGameBoard(size, initialVal) {
   }
   return obj;
 }
-
-
-function printCongratulation() {
-  console.log('')
-  console.log("||  ||   ||   ||  ||    ||    ||    ||   ||     ||  || || ||");
-  console.log(" ||||   ||||  ||  ||    ||    ||   ||||  ||||   ||  || || ||");
-  console.log("  ||   ||  || ||  ||    ||    ||  ||  || || ||  ||  || || ||");
-  console.log("  ||    ||||  ||  ||    || || ||   ||||  ||  || ||          ");
-  console.log("  ||     ||     ||       ||||||     ||   ||   ||||  || || ||");
-  console.log("");
-}
-
-
-function printTie() {
-  console.log("");
-  console.log("      |||      ||||||||  ||  ||||||   || || ||");
-  console.log("     || ||        ||     ||  ||       || || ||");
-  console.log("    ||   ||       ||     ||  |||      || || ||");
-  console.log("    |||||||       ||     ||  ||               ");
-  console.log("   ||     ||      ||     ||  ||||||   || || ||");
-  console.log("");
-}
-
 
 function computerChooseSquareHard(game, winCombination, sign) {
   let computerChoice = "";
@@ -283,7 +258,6 @@ function computerOffend(game, sign, winCombination) {
 }
 
 
-
 function computerDeffend(game, sign, winCombination) {
   let computerChoice;
   let otherPlayerSign = returnOtherPlayerSign(sign, game.currentPlayer.chosenSign);
@@ -328,74 +302,123 @@ function returnOtherPlayerSign(sign, currentPlayerSign) {
   }
 }
 
-// ///////////////
-let gameBoard = {
-  'a1': 'X',
-  'a2': 'X',
-  'a3': 'O',
-  'b1': 'O',
-  'b2': ' ',
-  'b3': ' ',
-  'c1': ' ',
-  'c2': 'X',
-  'c3': ' '
-};
+function computerChooseSquareExtreme(game, sign, winCombination) {
 
-currentPlayer = {
-  sign: 'X'
-}
-chooseBestMove(gameBoard, currentPlayer);
-
-let winningCombination = [];
-
-
-function chooseBestMove(board, currentPlayer) {
   let bestScore = -Infinity;
   let bestMove;
-  //iterate through all the move
-  for (let key in board) {
-    if (board[key] === ' ') {
-      board[key] = currentPlayer.sign; //place a sign 
-      score = minimax(gameBoard);
+  let maximizingPlayer = game.currentPlayer.chosenSign;
+  let minimizingPlayer = returnOtherPlayerSign(sign, game.currentPlayer.chosenSign);
+
+  for (let key in game.gameBoard) {
+
+    if (game.gameBoard[key] === sign.initialMarker) {
+      game.gameBoard[key] = game.currentPlayer.chosenSign;
+      game.takenSquares++;
+      let score = minimax(game, maximizingPlayer, 1, sign, winCombination, false, minimizingPlayer);
+
+      game.gameBoard[key] = sign.initialMarker;
+      game.takenSquares--;
 
       if (score > bestScore) {
+        bestScore = score;
         bestMove = key;
       }
     }
   }
-  //for each move call minimax()
-  //save minimax in the best Score 
-  //if bestSCore < minimax
-  //then save minimax as a BestScore
   return bestMove;
 }
 
-function minimax(gameBoard) {
-  //   //take the move and check if it's a win
-  //   // if win === true => return 1 and undo move
-  //   //if win === false => return -1 and undo move
-  //   //check if it's a tie return  => 0 and undo move 
-  //   // check all possible moves 
-  //   //if none call minimax()
-  //   // continue until no moves left 
-  console.log('ok');
-  return 1;
+
+function minimax(game, maximizingPlayer, depth, sign, winCombination, isMaximizing, minimizingPlayer) {
+
+  let valueOfMove = checkValueOfMove(game, maximizingPlayer, winCombination, minimizingPlayer);
+  if (valueOfMove !== null) {
+    return valueOfMove;
+  }
+
+  if (isMaximizing) {
+    let bestScore = -Infinity;
+
+    for (let key in game.gameBoard) {
+      if (game.gameBoard[key] === sign.initialMarker) {
+        game.gameBoard[key] = maximizingPlayer;
+        game.takenSquares++;
+
+        let score = minimax(game, maximizingPlayer, depth + 1, sign, winCombination, false, minimizingPlayer);
+
+        game.gameBoard[key] = sign.initialMarker;
+        game.takenSquares--;
+        bestScore = Math.max(score, bestScore);
+      }
+    }
+    return bestScore;
+
+  } else {
+    let bestScore = Infinity;
+    for (let key in game.gameBoard) {
+      if (game.gameBoard[key] === sign.initialMarker) {
+        game.gameBoard[key] = minimizingPlayer;
+        game.takenSquares++;
+        let score = minimax(game, maximizingPlayer, depth + 1, sign, winCombination, true, minimizingPlayer);
+        game.gameBoard[key] = sign.initialMarker;
+        game.takenSquares--;
+        bestScore = Math.min(score, bestScore);
+      }
+    }
+    return bestScore;
+  }
 }
 
 
-///////////////////////////////////////
+function isWinning(game, winCombination, chosenSign) {
+  for (let line = 0; line < winCombination.length; line++) {
+    let [sq1, sq2, sq3] = winCombination[line];
+
+    if (
+      game.gameBoard[sq1] === chosenSign &&
+      game.gameBoard[sq2] === chosenSign &&
+      game.gameBoard[sq3] === chosenSign
+    ) {
+      return true;
+    }
+  }
+  return false;
+}
 
 
+function checkValueOfMove(game, maximizingPlayer, winCombination, minimizingPlayer) {
+
+  if (isWinning(game, winCombination, maximizingPlayer)) {
+    return 10;
+  } else if (isWinning(game, winCombination, minimizingPlayer)) {
+    return -10;
+  } else if (checkIfTie(game)) {
+    return 0;
+  } else {
+    return null;
+  }
+}
 
 
+function printCongratulation() {
+  console.log('')
+  console.log("||    ||    ||   ||     ||  || || ||");
+  console.log("||    ||    ||   ||||   ||  || || ||");
+  console.log("||    ||    ||   || ||  ||  || || ||");
+  console.log("|| || ||    ||   ||  || ||          ");
+  console.log(" ||||||     ||   ||   ||||  || || ||");
+  console.log("");
+}
 
 
-
-
-module.exports.computerChooseSquareExtreme = computerChooseSquareExtreme;
-
-function computerChooseSquareExtreme(board, game) {
-  console.log('computer move extreme');
+function printTie() {
+  console.log("");
+  console.log("      |||      ||||||||  ||  ||||||   || || ||");
+  console.log("     || ||        ||     ||  ||       || || ||");
+  console.log("    ||   ||       ||     ||  |||      || || ||");
+  console.log("    |||||||       ||     ||  ||               ");
+  console.log("   ||     ||      ||     ||  ||||||   || || ||");
+  console.log("");
 }
 
 
@@ -427,45 +450,7 @@ module.exports.computerOffense = computerOffend;
 module.exports.computerDeffense = computerDeffend;
 module.exports.computerChooseSquareExtreme = computerChooseSquareExtreme;
 module.exports.returnOtherPlayerSign = returnOtherPlayerSign;
+module.exports.minimax = minimax;
+module.exports.checkValueOfMove = checkValueOfMove;
+module.exports.isWinning = isWinning;
 
-
-//working on now
-// function computerMove(game, winCombination, board) {
-//   if (game.takenSquares < 2) {
-//     let firstMove = ['a1', 'a3', 'b2', 'c1', 'c3'];
-//     let computerChoice = randomChooseBetween(firstMove);
-//     while (!checkIfEmpty(computerChoice, board)) {
-//       computerChoice = randomChooseBetween(firstMove);
-//       placeSignToBoard(computerChoice, board, game);
-//     }
-//   } else if (game.takenSquares > 2) {
-//     computerChoice = chooseIfChanceToWin();
-//     if (computerChoice) {
-//       computerChoice = computerDefend();
-//     }
-//     placeSignToBoard(computerChoice, board, game);
-//   }
-// }
-
-
-// working on  now
-// function chooseIfChanceToWin(board, winCombination) {
-
-//   for (let line = 0; line < winCombination.length; line++) {
-//     let [sq1, sq2, sq3] = winCombination[line];
-
-//     if (
-//       (board[sq1] === gameNow.currentPlayer.chosenSign &&
-//         board[sq2] === gameNow.currentPlayer.chosenSign)
-//       ||
-//       (board[sq1] === gameNow.currentPlayer.chosenSign &&
-//         board[sq3] === gameNow.currentPlayer.chosenSign)
-//       ||
-//       (board[sq2] === gameNow.currentPlayer.chosenSign &&
-//         board[sq3] === gameNow.currentPlayer.chosenSign)
-//     ) {
-//       return true;
-//     }
-//   }
-//   return false;
-// }
--- tic_tac_toe_console.js
+++ tic_tac_toe_console.js
@@ -1,9 +1,9 @@
 const readline = require('readline-sync');
-const messages = require('./messages_tic_tac_toe.json');
+const MESSAGES = require('./messages_tic_tac_toe.json');
 const VALID_ANSWERS = require('./valid_answers.json');
 const HELPERS = require('./helpers.js');
 
-let signs = {
+const SIGNS = {
   initialMarker: ' ',
   cross: 'X',
   circle: 'O'
@@ -15,19 +15,19 @@ const WIN_COMBINATION = [
   ['a1', 'b2', 'c3'], ['c1', 'b2', 'a3']
 ];
 
-let players = {
+const PLAYERS = {
   player1: {
     id: 'player1',
     name: 'player1',
     human: true,
-    chosenSign: signs.cross,
+    chosenSign: SIGNS.cross,
     points: 0
   },
   player2: {
     id: 'player2',
     name: 'player2',
     human: false,
-    chosenSign: signs.circle,
+    chosenSign: SIGNS.circle,
     points: 0
   }
 };
@@ -38,38 +38,38 @@ let gameStatus = {
   takenSquares: 0,
   round: 1,
   level: 1,
-  currentPlayer: players.player1,
+  currentPlayer: PLAYERS.player1,
   gameBoard: {
-    a1: signs.initialMarker,
-    a2: signs.initialMarker,
-    a3: signs.initialMarker,
-    b1: signs.initialMarker,
-    b2: signs.initialMarker,
-    b3: signs.initialMarker,
-    c1: signs.initialMarker,
-    c2: signs.initialMarker,
-    c3: signs.initialMarker
+    a1: SIGNS.initialMarker,
+    a2: SIGNS.initialMarker,
+    a3: SIGNS.initialMarker,
+    b1: SIGNS.initialMarker,
+    b2: SIGNS.initialMarker,
+    b3: SIGNS.initialMarker,
+    c1: SIGNS.initialMarker,
+    c2: SIGNS.initialMarker,
+    c3: SIGNS.initialMarker
   }
 }
 
 
 HELPERS.clearScreen();
-HELPERS.print(messages.welcome, ' ');
+HELPERS.print(MESSAGES.welcome, ' ');
 
 gameStatus.currentPlayer.name = readline.question('What is your name?');
 
-let playWith = HELPERS.retriveInput(messages.player, messages.playWithValidity, VALID_ANSWERS.validNums, gameStatus.currentPlayer.name);
-HELPERS.updateWhoIsPlaying(playWith, players.player2);
+let playWith = HELPERS.retriveInput(MESSAGES.player, MESSAGES.playWithValidity, VALID_ANSWERS.validNums, gameStatus.currentPlayer.name);
+HELPERS.updateWhoIsPlaying(playWith, PLAYERS.player2);
 
 if (playWith === '1') {
-  players.player2.name = readline.question('What is your name of player 2?');
+  PLAYERS.player2.name = readline.question('What is your name of player 2?');
 } else {
-  gameStatus.level = HELPERS.retriveInput(messages.level, messages.levelValidity, VALID_ANSWERS.validLevel, gameStatus.currentPlayer.name);
+  gameStatus.level = HELPERS.retriveInput(MESSAGES.level, MESSAGES.levelValidity, VALID_ANSWERS.validLevel, gameStatus.currentPlayer.name);
 }
 
-let player1chosenSign = HELPERS.retriveInput(messages.chooseSign, messages.signValidity, VALID_ANSWERS.validSigns, gameStatus.currentPlayer.name);
+let player1chosenSign = HELPERS.retriveInput(MESSAGES.chooseSign, MESSAGES.signValidity, VALID_ANSWERS.validSigns, gameStatus.currentPlayer.name);
 
-HELPERS.assignSignsToPlayers(player1chosenSign, players, signs);
+HELPERS.assignSignsToPlayers(player1chosenSign, PLAYERS, SIGNS);
 HELPERS.clearScreen();
 HELPERS.printBoard(gameStatus.gameBoard);
 
@@ -78,23 +78,22 @@ while (!gameStatus.win || !gameStatus.tie) {
   let squareId = '';
 
   if (gameStatus.currentPlayer.human) {
-    squareId = HELPERS.humanChooseSquare(messages, gameStatus.gameBoard, signs.initialMarker, VALID_ANSWERS, gameStatus.currentPlayer.name);
+    squareId = HELPERS.humanChooseSquare(MESSAGES, gameStatus.gameBoard, SIGNS.initialMarker, VALID_ANSWERS, gameStatus.currentPlayer.name);
 
   } else if (gameStatus.level === "1") {
-    squareId = HELPERS.computerChooseSquareEasy(gameStatus, signs);
+    squareId = HELPERS.computerChooseSquareEasy(gameStatus, SIGNS);
 
 
   } else if (gameStatus.level === "2") {
-    squareId = HELPERS.computerChooseSquareHard(gameStatus, WIN_COMBINATION, signs);
+    squareId = HELPERS.computerChooseSquareHard(gameStatus, WIN_COMBINATION, SIGNS);
 
   } else {
-    squareId = HELPERS.computerChooseSquareExtreme(gameStatus);
+    squareId = HELPERS.computerChooseSquareExtreme(gameStatus, SIGNS, WIN_COMBINATION);
   }
 
   HELPERS.clearScreen();
-  HELPERS.placeSignToBoard(messages, squareId, gameStatus, signs);
+  HELPERS.placeSignToBoard(MESSAGES, squareId, gameStatus, SIGNS);
   HELPERS.printBoard(gameStatus.gameBoard);
-
 
 
   gameStatus.win = HELPERS.checkIfWin(gameStatus, WIN_COMBINATION);
@@ -102,336 +101,27 @@ while (!gameStatus.win || !gameStatus.tie) {
 
     HELPERS.incrementPoints(gameStatus);
     HELPERS.printCongratulation();
-    HELPERS.announceGameStatus(messages.congratulation, players, gameStatus.currentPlayer.name);
+    HELPERS.announceGameStatus(MESSAGES.congratulation, PLAYERS, gameStatus.currentPlayer.name);
   } else if (!gameStatus.win) {
     gameStatus.tie = HELPERS.checkIfTie(gameStatus);
     if (gameStatus.tie) {
       HELPERS.printTie();
-      HELPERS.announceGameStatus(messages.tie, players, gameStatus.currentPlayer.name)
+      HELPERS.announceGameStatus(MESSAGES.tie, PLAYERS, gameStatus.currentPlayer.name)
     }
   }
 
   if (gameStatus.win || gameStatus.tie) {
-    let playAgain = HELPERS.retriveInput(messages.playAgain, messages.invalidYesNo, VALID_ANSWERS.validAnswers, gameStatus.currentPlayer.name);
+    let playAgain = HELPERS.retriveInput(MESSAGES.playAgain, MESSAGES.invalidYesNo, VALID_ANSWERS.validAnswers, ' ');
 
     if (playAgain.toLowerCase().slice(0, 1) === 'y') {
-
-      gameStatus.gameBoard = HELPERS.initializeGameBoard(3, signs.initialMarker);
-
-      HELPERS.restartGame(gameStatus, players);
-
+      gameStatus.gameBoard = HELPERS.initializeGameBoard(3, SIGNS.initialMarker);
+      HELPERS.restartGame(gameStatus, PLAYERS);
 
     } else {
-      HELPERS.print(messages.thankYou, gameStatus.currentPlayer.name);
+      HELPERS.print(MESSAGES.thankYou, gameStatus.currentPlayer.name);
       return;
     }
   } else {
-    HELPERS.changeTurn(gameStatus, messages, players);
+    HELPERS.changeTurn(gameStatus, MESSAGES, PLAYERS);
   }
-
 }
-
-
-
-
-//helper functions
-// function print(message, name) {
-//   console.log(`=> ${name}, ${message}`);
-// }
-
-// function validateInput(input, validInputs) {
-//   return validInputs.includes(input);
-// }
-
-
-// function retriveInput(question, notValidMessage, validInputs, name) {
-//   print(question, name);
-//   let userAnswer = readline.question('');
-//   let isValid = validateInput(userAnswer, validInputs);
-//   while (!isValid) {
-//     print(notValidMessage, name);
-//     print(question, name);
-//     userAnswer = readline.question('');
-//     isValid = validateInput(userAnswer, validInputs);
-//   }
-
-//   return userAnswer;
-// }
-
-// function updateWhoIsPlaying(userPlayWith, player2Obj) {
-//   if (userPlayWith === '1') {
-//     player2Obj.human = true;
-//   } else {
-//     player2Obj.human = false;
-//   }
-// }
-
-// function assignSignsToPlayers(initialchosenSign, player1Obj, player2Obj) {
-//   if (initialchosenSign === "x") {
-//     player1Obj.chosenSign = CROSS;
-//     player2Obj.chosenSign = CIRCLE;
-//   } else {
-//     player1Obj.chosenSign = CIRCLE;
-//     player2Obj.chosenSign = CROSS;
-//   }
-// }
-
-// function printBoard(board) {
-
-//   let line = '      ---+---+---';
-//   let numRow = `       1   2   3   `;
-//   let firstRow = `     a ${board.a1} | ${board.a2} | ${board.a3} `;
-//   let secondRow = `     b ${board.b1} | ${board.b2} | ${board.b3} `;
-//   let thirdRow = `     c ${board.c1} | ${board.c2} | ${board.c3} `;
-
-//   console.log(' ');
-//   console.log(numRow);
-//   console.log(firstRow);
-//   console.log(line);
-//   console.log(secondRow);
-//   console.log(line);
-//   console.log(thirdRow);
-//   console.log(' ');
-// }
-
-// function humanChooseSquare(messages, board, initialMarker, validAnswers, name) {
-//   let chosenSquare = retriveInput(messages.chooseSquare, messages.ivalidSquare, VALID_ANSWERS.validSquares, name);
-
-//   while (board[chosenSquare] !== initialMarker) {
-//     print(messages.emptySquare, name);
-//     chosenSquare = retriveInput(messages.chooseSquare, messages.ivalidSquare, validAnswers.validSquares);
-//   }
-//   return chosenSquare;
-// }
-
-// function checkIfEmpty(square, board) {
-//   return (board[square] === INITIAL_MARKER)
-// }
-
-// function placeSignToBoard(messages, square, board, game, name) {
-//   if (checkIfEmpty(square, board)) {
-//     board[square] = game.currentPlayer.chosenSign;
-//     game.takenSquares++;
-//   } else {
-//     print(messages.emptySquare, name);
-//   }
-// }
-
-// function checkIfWin(board, gameNow, winCombination) {
-
-//   for (let line = 0; line < winCombination.length; line++) {
-//     let [sq1, sq2, sq3] = winCombination[line];
-
-//     if (
-//       board[sq1] === gameNow.currentPlayer.chosenSign &&
-//       board[sq2] === gameNow.currentPlayer.chosenSign &&
-//       board[sq3] === gameNow.currentPlayer.chosenSign
-//     ) {
-//       return true;
-//     }
-//   }
-//   return false;
-// }
-
-// function checkIfTie(board) {
-//   if (board === 9) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
-
-// function incrementPoints(gameNow) {
-//   if (gameNow.win) {
-//     gameNow.currentPlayer.points++;
-//   }
-// }
-
-// function announceGameStatus(message, player1Score, player2Score, name) {
-//   print(message, name);
-//   console.log(`=> Player 1 has ${player1Score} points and Player 2 has ${player2Score} points`);
-// }
-
-// function changeTurn(game) {
-//   if (game.currentPlayer.id === 'player1') {
-//     game.currentPlayer = player2;
-//   } else {
-//     game.currentPlayer = player1;
-//   }
-//   print(messages.YourTurn, game.currentPlayer.name);
-// }
-
-
-// function computerChooseSquareEasy(board) {
-//   let squares = ['a1', 'a2', 'a3', 'b1', 'b2', 'b3', 'c1', 'c2', 'c3'];
-//   let computerChoice = randomChooseBetween(squares);
-//   while (!checkIfEmpty(computerChoice, board)) {
-//     computerChoice = randomChooseBetween(squares);
-//   }
-//   return computerChoice;
-// }
-
-// function randomChooseBetween(squares) {
-//   return squares[Math.floor(Math.random() * squares.length)];
-// }
-
-// function restartGame(board, game, firstPlayer, secondPlayer, name) {
-//   clearScreen();
-//   printBoard(board, name);
-//   resetGameStatus(game, name);
-//   game.round++;
-//   choosePlayerForFirstMove(firstPlayer, secondPlayer, game, board)
-// }
-
-// function choosePlayerForFirstMove(firstPlayer, secondPlayer, game, board) {
-//   if (game.round % 2 === 1) {
-//     board.currentPlayer = firstPlayer;
-//   } else {
-//     board.currentPlayer = secondPlayer;
-//   }
-// }
-
-// function clearScreen() {
-//   for (let i = 0; i < 22; i++) {
-//     console.log(" ");
-//   }
-// }
-
-// function resetGameStatus(game) {
-//   game.win = false;
-//   game.tie = false;
-//   game.takenSquares = 0;
-//   game.currentPlayer = player1;
-// }
-
-// function initializeGameBoard(size, initialVal) {
-//   let obj = {};
-//   let rows = ['a', 'b', 'c'];
-
-//   for (let col = 1; col <= size; col++) {
-//     for (let row = 0; row < size; row++) {
-//       obj[rows[row] + col] = initialVal;
-//     }
-//   }
-//   return obj;
-// }
-
-
-
-// //not implemented 
-// function printCongratulation() {
-//   console.log('')
-//   console.log("||  ||   ||   ||  ||    ||    ||    ||   ||     ||  || || ||");
-//   console.log(" ||||   ||||  ||  ||    ||    ||   ||||  ||||   ||  || || ||");
-//   console.log("  ||   ||  || ||  ||    ||    ||  ||  || || ||  ||  || || ||");
-//   console.log("  ||    ||||  ||  ||    || || ||   ||||  ||  || ||          ");
-//   console.log("  ||     ||     ||       ||||||     ||   ||   ||||  || || ||");
-//   console.log("");
-// }
-
-// function printTie() {
-//   console.log("");
-//   console.log("      |||      ||||||||  ||  ||||||   || || ||");
-//   console.log("     || ||        ||     ||  ||       || || ||");
-//   console.log("    ||   ||       ||     ||  |||      || || ||");
-//   console.log("    |||||||       ||     ||  ||               ");
-//   console.log("   ||     ||      ||     ||  ||||||   || || ||");
-//   console.log("");
-// }
-
-// //
-
-// function computerChooseSquareHard(board, game, winCombination, validSquares) {
-//   let computerChoice = undefined;
-//   if (game.takenSquares <= 2) {
-//     let strategicSquares = ['a1', 'a3', 'b2', 'c1', 'c3'];
-//     computerChoice = randomChooseBetween(strategicSquares);
-//     while (!checkIfEmpty(computerChoice, board)) {
-//       computerChoice = randomChooseBetween(strategicSquares);
-//     }
-//   } else if (game.takenSquares >= 3) {
-//     computerChoice = computerOffense(board, validSquares);
-//     if (!computerChoice) {
-//       computerDeffense(board, validSquares)
-//     } else {
-//       computerChoice = randomChooseBetween(strategicSquares);
-//       while (!checkIfEmpty(computerChoice, board)) {
-//         computerChoice = randomChooseBetween(strategicSquares);
-//       }
-//     }
-//   }
-// }
-
-// function computerOffense(board, validSquares) {
-//   let computerChoice;
-//   //loop through winningcombination
-//   //compare it to the current board
-//   //check if
-//   computerChoice = randomChooseBetween(validSquares);
-//   while (!checkIfEmpty(computerChoice, board)) {
-//     computerChoice = randomChooseBetween(validSquares);
-//   }
-//   console.log('computer offense');
-//   return computerChoice;
-// }
-
-// function computerDeffense(board, validSquares) {
-//   let computerChoice;
-//   //loop through winningcombination
-//   //compare it to the current board
-//   //check if
-//   computerChoice = randomChooseBetween(validSquares);
-//   while (!checkIfEmpty(computerChoice, board)) {
-//     computerChoice = randomChooseBetween(validSquares);
-//   }
-//   console.log('computer defense');
-//   return computerChoice;
-// }
-
-
-// function computerChooseSquareExtreme(board, game) {
-//   console.log('computer move extreme');
-// }
-
-
-//working on now
-// function computerMove(game, winCombination, board) {
-//   if (game.takenSquares < 2) {
-//     let firstMove = ['a1', 'a3', 'b2', 'c1', 'c3'];
-//     let computerChoice = randomChooseBetween(firstMove);
-//     while (!checkIfEmpty(computerChoice, board)) {
-//       computerChoice = randomChooseBetween(firstMove);
-//       placeSignToBoard(computerChoice, board, game);
-//     }
-//   } else if (game.takenSquares > 2) {
-//     computerChoice = chooseIfChanceToWin();
-//     if (computerChoice) {
-//       computerChoice = computerDefend();
-//     }
-//     placeSignToBoard(computerChoice, board, game);
-//   }
-// }
-
-
-// working on  now
-// function chooseIfChanceToWin(board, winCombination) {
-
-//   for (let line = 0; line < winCombination.length; line++) {
-//     let [sq1, sq2, sq3] = winCombination[line];
-
-//     if (
-//       (board[sq1] === gameNow.currentPlayer.chosenSign &&
-//         board[sq2] === gameNow.currentPlayer.chosenSign)
-//       ||
-//       (board[sq1] === gameNow.currentPlayer.chosenSign &&
-//         board[sq3] === gameNow.currentPlayer.chosenSign)
-//       ||
-//       (board[sq2] === gameNow.currentPlayer.chosenSign &&
-//         board[sq3] === gameNow.currentPlayer.chosenSign)
-//     ) {
-//       return true;
-//     }
-//   }
-//   return false;
-// }
-
